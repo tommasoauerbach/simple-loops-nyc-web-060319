16935afd714f9701a2eec46cb5edf656c7ef3d6c
tommaso auerbach <tsa302@nyu.edu>
2019-06-01 17:30:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,6 +10,9 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
+  7.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
