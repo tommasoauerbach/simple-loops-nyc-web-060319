e67e6f2f9782d4060f1576893e503958c9595981
tommaso auerbach <tsa302@nyu.edu>
2019-06-01 17:34:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -32,7 +32,7 @@ end
 
 def until_iterator(number_of_times)
   counter = 0
-  until counter == 8
+  until counter == 7
     puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1
   end
